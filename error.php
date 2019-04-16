aa3e4f10577d1ccb3d5b19605df53eeb0306e778
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-04-16 18:43:07 +0800

    v1.1 shipped

---
+++ error.php
@@ -0,0 +1,8 @@
+<?php
+switch($_GET['code'])
+{
+case '404':header('Location: https://www.maroquinerie-latitude.fr/');
+break;
+default:header('Location: https://www.maroquinerie-latitude.fr/');
+}
+?>
